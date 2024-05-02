d24d5f7aa46fdbf333d22e270c1413e58006dcd6
catcherwong <catcher_hwq@outlook.com>
2024-05-02 05:37:58 +0000

    add aws cognito with iot core sample
    Signed-off-by: catcherwong <catcher_hwq@outlook.com>

---
+++ AwsCognitoAndIotCoreSample/Backend/Controllers/UserController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody]LoginDto dto)
+        {
+            if (dto != null && dto.Username == "admin" && dto.Password == "admin")
+            {
+                return Ok(new 
+                {
+                    user_id = Guid.NewGuid().ToString("N"),
+                    token = Guid.NewGuid().ToString("N")
+                });
+            }
+            else
+            {
+                return Unauthorized("Login Failed");
+            }
+        }
+
+        private const string CognitoIdentityPoolId = "us-west-2:xxxxxxx";
+        private const string CognitoIdentityProviderName = "myprovider";
+        private const string IotCoreEndpoint = "xxxxxx.iot.us-west-2.amazonaws.com";
+
+        [HttpGet("getIot")]
+        public async Task<IActionResult> GetIotCoreInfo([FromHeader(Name = "B-Token")] string token)
+        {
+            if(string.IsNullOrWhiteSpace(token))
+            {
+                return Unauthorized("Token is required");
+            }
+
+            // will read ak/sk from env
+            Amazon.CognitoIdentity.AmazonCognitoIdentityClient client = new Amazon.CognitoIdentity.AmazonCognitoIdentityClient();
+
+           var resp = await client.GetOpenIdTokenForDeveloperIdentityAsync(new Amazon.CognitoIdentity.Model.GetOpenIdTokenForDeveloperIdentityRequest
+            {
+                IdentityPoolId = CognitoIdentityPoolId,
+                Logins = new Dictionary<string, string>
+                {
+                    { CognitoIdentityProviderName, token }
+                }
+            });
+
+            // will read ak/sk from env
+            Amazon.IoT.AmazonIoTClient iotClient = new Amazon.IoT.AmazonIoTClient();
+
+            var iotPolicy = @"{
+    ""Version"": ""2012-10-17"",
+    ""Statement"": [
+        {
+            ""Effect"": ""Allow"",
+            ""Action"": [
+                ""iot:Publish"",
+                ""iot:Subscribe"",
+                ""iot:Receive""
+            ],
+            ""Resource"": [""arn:aws:iot:*:*:topic/user/${cognito-identity.amazonaws.com:sub}""]
+        },
+        {
+            ""Effect"": ""Allow"",
+            ""Action"": [
+                ""iot:Connect""
+            ],
+            ""Resource"": [""arn:aws:iot:*:*:client/${cognito-identity.amazonaws.com:sub}""]
+        }
+    ]
+}";
+
+            _ =  await iotClient.CreatePolicyAsync(new Amazon.IoT.Model.CreatePolicyRequest
+            {
+                PolicyName = "CognitoIotCorePolicy",
+                PolicyDocument = iotPolicy
+            });
+
+
+            var attachResp = await iotClient.AttachPolicyAsync(new Amazon.IoT.Model.AttachPolicyRequest
+            {
+                PolicyName = "CognitoIotCorePolicy",
+                Target = resp.IdentityId
+            });
+
+
+            return Ok(new 
+            {
+                iot_endpoint = IotCoreEndpoint,
+                client_id = resp.IdentityId,
+            });
+
+        }
+
+        public class LoginDto
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+    }
+
+
+}
---
+++ AwsCognitoAndIotCoreSample/IotClient/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet.Client;
+using MQTTnet;
+using Newtonsoft.Json;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+// 1. login
+HttpClient httpClient = new();
+httpClient.BaseAddress = new Uri("http://localhost:7878");
+
+var loginResp = await httpClient.PostAsync("login", new StringContent(JsonConvert.SerializeObject(new { Username = "admin", Password = "admin" }), Encoding.UTF8));
+var loginRespContent = await loginResp.Content.ReadAsStringAsync();
+var loginRespObj = JsonConvert.DeserializeObject<LoginResponse>(loginRespContent);
+
+// 2. getIot
+HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "getIot");
+request.Headers.Add("B-Token", loginRespObj.token);
+var iotResp = await httpClient.SendAsync(request);
+var iotrespContent = await iotResp.Content.ReadAsStringAsync();
+var iotRespObj = JsonConvert.DeserializeObject<IotResponse>(iotrespContent);
+
+// 3. connect to mqtt with websocket
+var factory = new MqttFactory();
+var mqttClient = factory.CreateMqttClient();
+
+var options = new MqttClientOptionsBuilder()
+    .WithClientId(iotRespObj.client_id)
+    .WithWebSocketServer(x => 
+    {
+        x.WithUri($"wss://{iotRespObj.iot_endpoint}/mqtt");
+    })
+    .Build();
+
+await mqttClient.ConnectAsync(options);
+
+await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic($"user/{iotRespObj.client_id}").Build());
+
+await mqttClient.PublishAsync(new MqttApplicationMessageBuilder()
+       .WithTopic($"user/{iotRespObj.client_id}")
+          .WithPayload("Hello World").WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtMostOnce)
+                   .Build());
+
+
+internal class IotResponse
+{
+    public string iot_endpoint { get; set; }
+    public string client_id { get; set; }
+
+}
+
+public class LoginResponse
+{
+    public string user_id { get; set; }
+    public string token { get; set; }
+}
